db15077678c7dbde5c70286b7a601a9cf06108f9
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2016-11-18 00:04:07 +0900

    add object profile

--- fluentd.rb
+++ fluentd.rb
@@ -27,8 +27,39 @@ Plugin::create(:fluentd) do
     end
   end
 
+  on_spectrum_set do |spectrum|
+    @lock << spectrum
+  end
+
   settings 'fluentd' do
     input "fluentd host", :fluentd_host
     adjustment "port number", :fluentd_port, 1024, 0xffff
   end
+
+  def object_counts
+    ObjectSpace.garbage_collect
+    objects = Hash.new(0)
+    ObjectSpace.each_object.to_a.each{|o| objects[o.class] += 1 unless (o.irregulareval? rescue nil) }
+    @logger.post("memory", objects)
+    objects.clear
+    profile
+  end
+
+  def profile
+    t = Reserver.new(1){ Plugin.call(:spectrum_set, -> { object_counts }) }
+    notice "profile time: #{t}"
+  end
+
+  @lock = Queue.new
+  Thread.new do
+    loop do
+      begin
+        @lock.pop.call
+      rescue => e
+        error e
+      end
+    end
+  end
+
+  profile()
 end
